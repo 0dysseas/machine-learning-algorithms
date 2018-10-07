f9b1f336452fb3c769eb4950ede6c9e51f8b4535
0dysseas <kaziolas.od@gmail.com>
2018-10-07 19:00:53 +0300

    Implemented Regularized Cost Function

--- logistic-regression/machine-learning-ex2/ex2/costFunctionReg.m
+++ logistic-regression/machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,10 +17,14 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+hypothesis = sigmoid(X*theta);
 
+J = -(1/m) * sum(y.*log(hypothesis) + (1-y).*log(1-hypothesis)) + (lambda/(2*m))*sum(theta(2:length(theta)).^2);
 
+grad(1) = (1/m)*sum((hypothesis-y).*X(:,1));
+grad_rest = (1/m)*sum((hypothesis-y).*X(:, 1)) + (lambda/m).*theta(2:length(theta));
 
-
+grad = [grad(1); grad_rest];
 
 % =============================================================
 
